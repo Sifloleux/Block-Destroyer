7863b6e321d3015223392db263ab524ee43ac2c0
Sifloleux <82200361+Sifloleux@users.noreply.github.com>
2021-05-30 14:51:52 +0200

    Added blocks sketch

--- Game.py
+++ Game.py
@@ -5,7 +5,7 @@ import random
 import os.path
 import math
 
-SCREEN_WIDTH = 630
+SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 710
 WHITE = (255, 255, 255)
 RED   = (255, 0, 0)
@@ -13,6 +13,8 @@ BLACK = (0, 0, 0)
 FPS = 60
 SHOOTING_SPEED = 6
 SPEED = [0, 0]
+BLOCK_MOVEMENT = 30
+
 starting_point = [SCREEN_WIDTH/2,SCREEN_HEIGHT-60]
 draw_line = 0
 x = [SCREEN_WIDTH/2,SCREEN_HEIGHT-60]
@@ -20,6 +22,17 @@ y=[float('nan'),float('nan')]
 mouse_draging = False
 shoot = 0
 num_of_balls = 1
+
+game_map = [[0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],]
+
+
 def loadImage(name, useColorKey=False):
     fullname = os.path.join("data",name)
     image = pygame.image.load(fullname)  
@@ -30,15 +43,18 @@ def loadImage(name, useColorKey=False):
     return image
 
 def get_speed(starting_point,pointer_point):
-    hypotenuse = math.sqrt((abs(pointer_point[1]-starting_point[1]))**2+(abs(pointer_point[0]-starting_point[0]))**2)
+    hypotenuse = math.sqrt((abs(pointer_point[1]-starting_point[1]))**2 + \
+                           (abs(pointer_point[0]-starting_point[0]))**2)
+        
     if starting_point[0] > pointer_point[0]:
         speed = [(pointer_point[0]-starting_point[0])/hypotenuse \
                  ,-(starting_point[1]-pointer_point[1])/hypotenuse]
-        print(speed)
+            
     elif starting_point[0] < pointer_point[0]:
          speed = [(pointer_point[0]-starting_point[0])/hypotenuse \
-                 ,-(starting_point[1]-pointer_point[1])/hypotenuse]  
-         print(speed)
+                 ,-(starting_point[1]-pointer_point[1])/hypotenuse]     
+    else:
+        speed=[0,1]
     return speed
 
 
@@ -47,9 +63,16 @@ def add_ball(mouse_position, SHOOTING_SPEED,starting_point):
     ball1 = Ball(RED,20,20,[SHOOTING_SPEED*speed[0],SHOOTING_SPEED*speed[1]])
     ball1.rect.x = starting_point[0] - 10
     ball1.rect.y = starting_point[1] - 10
-    all_sprites_list.add(ball1)
-    all_sprites_list.draw(screen)
+    all_balls_list.add(ball1)
+    all_balls_list.draw(screen)
     pygame.display.flip()
+
+
+
+
+#def add_block_line()
+
+
     
 class Ball(pygame.sprite.Sprite):
 
@@ -69,16 +92,58 @@ class Ball(pygame.sprite.Sprite):
         if self.rect.bottom >= SCREEN_HEIGHT - 50:
             self.kill()
 
+class Block(pygame.sprite.Sprite):
+    
+    def __init__(self, color, width, height,hp):
+        super().__init__()
+        self.hp = hp
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+        pygame.draw.rect(self.image, color, [0, 0, SCREEN_WIDTH/10, SCREEN_WIDTH/10])        
+        self.rect = self.image.get_rect()
+        #self.rect.bottom.
+    
+    def update(self):
+        self.rect.y += BLOCK_MOVEMENT
+        if self.hp <=0:
+             self.kill()
+             
+             
+class Line(pygame.sprite.Sprite):
+     def __init__(self, color, width, height,hp):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+        pygame.draw.rect(self.image, color, [0, 0, width, height],3)        
+        self.rect = self.image.get_rect()
+        #self.rect.bottom.
+    
+     def update(self):
+         self.rect.y += BLOCK_MOVEMENT
+         if self.hp <=0:
+             self.kill()
+             
+             
+             
+             
 #----------------------------
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 mouse_draging = False
+all_balls_list = pygame.sprite.Group()
+all_blocks_list = pygame.sprite.Group()
+all_vertical_list = pygame.sprite.Group()
+all_horizontal_list = pygame.sprite.Group()
+ 
 
-all_sprites_list = pygame.sprite.Group()
- 
 carryOn = True
- 
+
+block = Block(WHITE,50,50,1)
+block.rect.x = 300
+block.rect.y = 100
+all_blocks_list.add(block)
 
 clock = pygame.time.Clock()
  
@@ -118,7 +183,7 @@ while carryOn:
             mouse_x, mouse_y = event.pos             
             draw_line = 1
             shoot = 1
-            all_sprites_list.draw(screen)  
+            all_balls_list.draw(screen)  
             pygame.display.flip()
                                    
         elif event.type == pygame.MOUSEMOTION:
@@ -129,10 +194,10 @@ while carryOn:
                 draw_line = 1
  
 
-    all_sprites_list.update()
+    all_balls_list.update()
  
 
-    for obj in all_sprites_list:
+    for obj in all_balls_list:
         if obj.rect.x>=SCREEN_WIDTH:
             obj.velocity[0] = -obj.velocity[0]
         if obj.rect.x<=0:
@@ -141,15 +206,28 @@ while carryOn:
             obj.velocity[1] = -obj.velocity[1]
         if obj.rect.y<0:
             obj.velocity[1] = -obj.velocity[1] 
- 
+
+    
+    for hit in pygame.sprite.groupcollide(all_balls_list,all_blocks_list,0,0):
+        hit.velocity[0] = - hit.velocity[0]
+        hit.velocity[1] = - hit.velocity[1]
+        
+    for hit in pygame.sprite.groupcollide(all_blocks_list,all_balls_list,0,0):
+        hit.hp = hit.hp -1
+        if hit.hp <=0:
+            hit.kill()
+        print(hit.hp)
+    
     screen.fill(BLACK)
     
 
-    all_sprites_list.draw(screen) 
+    all_balls_list.draw(screen) 
+    all_blocks_list.draw(screen) 
     pygame.draw.circle(screen, RED, [x[0],x[1]],5)
+    #if not all_balls_list:
 
     if draw_line == 1:
-        pygame.draw.line(screen, 'blue', starting_point,[mouse_x, mouse_y] , 2)
+        pygame.draw.line(screen, (200,200,200), starting_point,[mouse_x, mouse_y])
     pygame.draw.line(screen,WHITE,[0,655],[SCREEN_WIDTH,655],2)
     pygame.display.flip()
 
