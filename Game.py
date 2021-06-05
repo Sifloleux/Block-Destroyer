81b71faee127318d84da06859b934e465843174c
Sifloleux <82200361+Sifloleux@users.noreply.github.com>
2021-06-05 11:36:14 +0200

    Added sketch of escape and mian menu

--- Game.py
+++ Game.py
@@ -11,11 +11,15 @@ WHITE = (255, 255, 255)
 RED   = (255, 0, 0)
 BLACK = (0, 0, 0)
 FPS = 60
-SHOOTING_SPEED = 20
+SHOOTING_SPEED = 4
 SPEED = [0, 0]
 BLOCK_MOVEMENT = SCREEN_WIDTH/10
 MENU_BAR_HEIGHT = 655
 menu_height = SCREEN_HEIGHT - MENU_BAR_HEIGHT
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 starting_point = [SCREEN_WIDTH/2,SCREEN_HEIGHT-60]
 draw_line = 0
@@ -28,10 +32,15 @@ NUM_of_balls = 4
 block_update_time = 0
 shot_already = 0
 difficulty = 5
-
 coins = 0
 game_on = 1
 game_on_ = 1
+main_menu = True
+game_screen = False
+escape_menu = False
+
+set_first_rows = 1
+
 
 def loadImage(name, useColorKey=False):
     fullname = os.path.join("data",name)
@@ -41,6 +50,11 @@ def loadImage(name, useColorKey=False):
         colorkey = image.get_at((0,0)) 
         image.set_colorkey(colorkey,RLEACCEL) 
     return image
+
+def loadSound(name):
+    fullname = os.path.join("data",name)
+    sound = pygame.mixer.Sound(fullname)
+    return sound
 
 def get_speed(starting_point,pointer_point):
     hypotenuse = math.sqrt((abs(pointer_point[1]-starting_point[1]))**2 + \
@@ -86,7 +100,7 @@ def add_block_line(SCREEN_WIDTH,difficulty):
                 
                 line_b = hor_Line(RED, 1)
                 line_b.rect.x = (SCREEN_WIDTH/10) * i 
-                line_b.rect.y = (SCREEN_WIDTH/10) - 2
+                line_b.rect.y = (SCREEN_WIDTH/10) - 1
                 
                 line_c = ver_Line(RED, 1)
                 line_c.rect.x = (SCREEN_WIDTH/10) * i -1
@@ -311,16 +325,15 @@ class hor_Line(pygame.sprite.Sprite):
      def __init__(self, color,hp):
         super().__init__()
         self.hp = hp
-        self.image = pygame.Surface([SCREEN_WIDTH/10, 2])
+        self.image = pygame.Surface([SCREEN_WIDTH/10 -1, 2])
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
-        pygame.draw.rect(self.image, BLACK, [0, 0, SCREEN_WIDTH/10, 2])   
+        pygame.draw.rect(self.image, RED, [0, 0, SCREEN_WIDTH/10 -1, 2])   
         self.rect = self.image.get_rect()
     
      def update(self):
          self.rect.y += BLOCK_MOVEMENT
-         if self.hp <=0:
-             self.kill()
+
              
 
           
@@ -331,13 +344,12 @@ class ver_Line(pygame.sprite.Sprite):
         self.image = pygame.Surface([2, SCREEN_WIDTH/10 -1])
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
-        pygame.draw.rect(self.image, BLACK, [0, 0, 2, SCREEN_WIDTH/10 -1])   
+        pygame.draw.rect(self.image, WHITE, [0, 0, 2, SCREEN_WIDTH/10 -1])   
         self.rect = self.image.get_rect()
     
      def update(self):
          self.rect.y += BLOCK_MOVEMENT
-         if self.hp <=0:
-             self.kill()
+
              
 
 
@@ -451,10 +463,10 @@ class Coin(pygame.sprite.Sprite):
     def update(self):
         self.rect.y += BLOCK_MOVEMENT
 
-    
-#----------------------------
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+
+
 mouse_draging = False
 all_balls_list = pygame.sprite.Group()
 all_blocks_list = pygame.sprite.Group()
@@ -465,149 +477,201 @@ destroyed = pygame.sprite.Group()
 collision_x = pygame.sprite.Group()
 collision_y = pygame.sprite.Group()
 
-
-carryOn = True
-#-----starting blocks
-row_0 = random.sample(range(10),4)
-row_1 = random.sample(range(10),4)
-row_2 = random.sample(range(10),4)
-row_3 = random.sample(range(10),4)
-
-for i in row_0: 
-    block = Block(WHITE,(SCREEN_WIDTH/10)-1,(SCREEN_WIDTH/10)-1,3)
-    block.rect.x = (SCREEN_WIDTH/10) * i
-    block.rect.y = (SCREEN_WIDTH/10) + 1
-    
-    line_a = hor_Line(RED, 1)
-    line_a.rect.x = (SCREEN_WIDTH/10) * i 
-    line_a.rect.y = (SCREEN_WIDTH/10) + 1
-    
-    line_b = hor_Line(RED, 1)
-    line_b.rect.x = (SCREEN_WIDTH/10) * i 
-    line_b.rect.y = (SCREEN_WIDTH/10) + (SCREEN_WIDTH/10) - 2
-    
-    line_c = ver_Line(RED, 1)
-    line_c.rect.x = (SCREEN_WIDTH/10) * i -1
-    line_c.rect.y = (SCREEN_WIDTH/10) +1
-    
-    line_d = ver_Line(RED, 1)
-    line_d.rect.x = (SCREEN_WIDTH/10) * (i+1) -2
-    line_d.rect.y = (SCREEN_WIDTH/10) + 1
-    
-    all_vertical_list.add(line_c)
-    all_vertical_list.add(line_d)
-    all_horizontal_list.add(line_a)
-    all_horizontal_list.add(line_b)
-    all_blocks_list.add(block)
-    
-for i in row_1: 
-    block = Block(WHITE,(SCREEN_WIDTH/10)-1,(SCREEN_WIDTH/10)-1,4)
-    block.rect.x = (SCREEN_WIDTH/10) * i
-    block.rect.y = 1
-    
-    line_a = hor_Line(RED, 1)
-    line_a.rect.x = (SCREEN_WIDTH/10) * i 
-    line_a.rect.y = 1
-    
-    line_b = hor_Line(RED, 1)
-    line_b.rect.x = (SCREEN_WIDTH/10) * i 
-    line_b.rect.y = (SCREEN_WIDTH/10) - 2
-    
-    line_c = ver_Line(RED, 1)
-    line_c.rect.x = (SCREEN_WIDTH/10) * i -1
-    line_c.rect.y = 1
-    
-    line_d = ver_Line(RED, 1)
-    line_d.rect.x = (SCREEN_WIDTH/10) * (i+1) -2
-    line_d.rect.y = 1
-    
-    all_vertical_list.add(line_c)
-    all_vertical_list.add(line_d)
-    all_horizontal_list.add(line_a)
-    all_horizontal_list.add(line_b)
-    all_blocks_list.add(block)
-
-for i in row_2: 
-    block = Block(WHITE,(SCREEN_WIDTH/10)-1,(SCREEN_WIDTH/10)-1,2)
-    block.rect.x = (SCREEN_WIDTH/10) * i
-    block.rect.y = 2*(SCREEN_WIDTH/10) + 1
-    
-    line_a = hor_Line(RED, 1)
-    line_a.rect.x = (SCREEN_WIDTH/10) * i 
-    line_a.rect.y = 2*(SCREEN_WIDTH/10) + 1
-    
-    line_b = hor_Line(RED, 1)
-    line_b.rect.x = (SCREEN_WIDTH/10) * i 
-    line_b.rect.y = 3*(SCREEN_WIDTH/10) - 2
-    
-    line_c = ver_Line(RED, 1)
-    line_c.rect.x = (SCREEN_WIDTH/10) * i -1
-    line_c.rect.y = 2*(SCREEN_WIDTH/10) + 1
-    
-    line_d = ver_Line(RED, 1)
-    line_d.rect.x = (SCREEN_WIDTH/10) * (i+1) -2
-    line_d.rect.y = 2*(SCREEN_WIDTH/10) +1
-    
-    all_vertical_list.add(line_c)
-    all_vertical_list.add(line_d)
-    all_horizontal_list.add(line_a)
-    all_horizontal_list.add(line_b)
-    all_blocks_list.add(block)
-
-for i in row_3: 
-    block = Block(WHITE,(SCREEN_WIDTH/10)-1,(SCREEN_WIDTH/10)-1,1)
-    block.rect.x = (SCREEN_WIDTH/10) * i
-    block.rect.y = 3*(SCREEN_WIDTH/10) + 1
-    
-    line_a = hor_Line(RED, 1)
-    line_a.rect.x = (SCREEN_WIDTH/10) * i 
-    line_a.rect.y = 3*(SCREEN_WIDTH/10) + 1
-    
-    line_b = hor_Line(RED, 1)
-    line_b.rect.x = (SCREEN_WIDTH/10) * i 
-    line_b.rect.y = 4*(SCREEN_WIDTH/10) - 2
-    
-    line_c = ver_Line(RED, 1)
-    line_c.rect.x = (SCREEN_WIDTH/10) * i -1
-    line_c.rect.y = 3*(SCREEN_WIDTH/10) + 1
-    
-    line_d = ver_Line(RED, 1)
-    line_d.rect.x = (SCREEN_WIDTH/10) * (i+1) -2
-    line_d.rect.y = 3*(SCREEN_WIDTH/10) + 1
-    
-    all_vertical_list.add(line_c)
-    all_vertical_list.add(line_d)
-    all_horizontal_list.add(line_a)
-    all_horizontal_list.add(line_b)
-    all_blocks_list.add(block)
-    
-# ----x----x----starting blocks-------x------x-----
+Ball_Launch = pygame.mixer.Sound('SOUND/ball_launch.wav')
+Ball_Hit = pygame.mixer.Sound("SOUND/ball_hit.wav")
+Ball_Launch.set_volume(0.3)
+Ball_Hit.set_volume(0.1)
 
 clock = pygame.time.Clock()
  
-
+carryOn= True
 while carryOn:
-    if game_on==1:
-        
-
+    if main_menu:
+        screen.fill(WHITE)
+        pygame.display.flip()
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
                   carryOn = False 
+                  
+            elif event.type == KEYDOWN:
+                if event.key == K_RETURN:
+                    print('xx')
+                    main_menu = False
+                    game_screen = True
+                    escape_menu = False
+    
+    
+    if escape_menu:
+        screen.fill(RED)
+        pygame.display.flip()
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                  carryOn = False 
+                  
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    print('xx')
+                    main_menu = False
+                    game_screen = True
+                    escape_menu = False
+                elif event.key == K_RETURN:
+                    print('xx')
+                    main_menu = True
+                    game_screen = False
+                    escape_menu = False
+    #-----starting blocks
+    elif set_first_rows:
+        row_0 = random.sample(range(10),4)
+        row_1 = random.sample(range(10),4)
+        row_2 = random.sample(range(10),4)
+        row_3 = random.sample(range(10),4)
+        
+        for i in row_0: 
+            block = Block(WHITE,(SCREEN_WIDTH/10)-1,(SCREEN_WIDTH/10)-1,3)
+            block.rect.x = (SCREEN_WIDTH/10) * i
+            block.rect.y = (SCREEN_WIDTH/10) + 1
+            
+            line_a = hor_Line(RED, 1)
+            line_a.rect.x = (SCREEN_WIDTH/10) * i 
+            line_a.rect.y = (SCREEN_WIDTH/10) + 1
+            
+            line_b = hor_Line(RED, 1)
+            line_b.rect.x = (SCREEN_WIDTH/10) * i 
+            line_b.rect.y = (SCREEN_WIDTH/10) + (SCREEN_WIDTH/10) - 2
+            
+            line_c = ver_Line(RED, 1)
+            line_c.rect.x = (SCREEN_WIDTH/10) * i -1
+            line_c.rect.y = (SCREEN_WIDTH/10) +1
+            
+            line_d = ver_Line(RED, 1)
+            line_d.rect.x = (SCREEN_WIDTH/10) * (i+1) -2
+            line_d.rect.y = (SCREEN_WIDTH/10) + 1
+            
+            all_vertical_list.add(line_c)
+            all_vertical_list.add(line_d)
+            all_horizontal_list.add(line_a)
+            all_horizontal_list.add(line_b)
+            all_blocks_list.add(block)
+            
+        for i in row_1: 
+            block = Block(WHITE,(SCREEN_WIDTH/10)-1,(SCREEN_WIDTH/10)-1,4)
+            block.rect.x = (SCREEN_WIDTH/10) * i
+            block.rect.y = 1
+            
+            line_a = hor_Line(RED, 1)
+            line_a.rect.x = (SCREEN_WIDTH/10) * i 
+            line_a.rect.y = 1
+            
+            line_b = hor_Line(RED, 1)
+            line_b.rect.x = (SCREEN_WIDTH/10) * i 
+            line_b.rect.y = (SCREEN_WIDTH/10) - 2
+            
+            line_c = ver_Line(RED, 1)
+            line_c.rect.x = (SCREEN_WIDTH/10) * i -1
+            line_c.rect.y = 1
+            
+            line_d = ver_Line(RED, 1)
+            line_d.rect.x = (SCREEN_WIDTH/10) * (i+1) -2
+            line_d.rect.y = 1
+            
+            all_vertical_list.add(line_c)
+            all_vertical_list.add(line_d)
+            all_horizontal_list.add(line_a)
+            all_horizontal_list.add(line_b)
+            all_blocks_list.add(block)
+        
+        for i in row_2: 
+            block = Block(WHITE,(SCREEN_WIDTH/10)-1,(SCREEN_WIDTH/10)-1,2)
+            block.rect.x = (SCREEN_WIDTH/10) * i
+            block.rect.y = 2*(SCREEN_WIDTH/10) + 1
+            
+            line_a = hor_Line(RED, 1)
+            line_a.rect.x = (SCREEN_WIDTH/10) * i 
+            line_a.rect.y = 2*(SCREEN_WIDTH/10) + 1
+            
+            line_b = hor_Line(RED, 1)
+            line_b.rect.x = (SCREEN_WIDTH/10) * i 
+            line_b.rect.y = 3*(SCREEN_WIDTH/10) - 2
+            
+            line_c = ver_Line(RED, 1)
+            line_c.rect.x = (SCREEN_WIDTH/10) * i -1
+            line_c.rect.y = 2*(SCREEN_WIDTH/10) + 1
+            
+            line_d = ver_Line(RED, 1)
+            line_d.rect.x = (SCREEN_WIDTH/10) * (i+1) -2
+            line_d.rect.y = 2*(SCREEN_WIDTH/10) +1
+            
+            all_vertical_list.add(line_c)
+            all_vertical_list.add(line_d)
+            all_horizontal_list.add(line_a)
+            all_horizontal_list.add(line_b)
+            all_blocks_list.add(block)
+        
+        for i in row_3: 
+            block = Block(WHITE,(SCREEN_WIDTH/10)-1,(SCREEN_WIDTH/10)-1,1)
+            block.rect.x = (SCREEN_WIDTH/10) * i
+            block.rect.y = 3*(SCREEN_WIDTH/10) + 1
+            
+            line_a = hor_Line(RED, 1)
+            line_a.rect.x = (SCREEN_WIDTH/10) * i 
+            line_a.rect.y = 3*(SCREEN_WIDTH/10) + 1
+            
+            line_b = hor_Line(RED, 1)
+            line_b.rect.x = (SCREEN_WIDTH/10) * i 
+            line_b.rect.y = 4*(SCREEN_WIDTH/10) - 2
+            
+            line_c = ver_Line(RED, 1)
+            line_c.rect.x = (SCREEN_WIDTH/10) * i -1
+            line_c.rect.y = 3*(SCREEN_WIDTH/10) + 1
+            
+            line_d = ver_Line(RED, 1)
+            line_d.rect.x = (SCREEN_WIDTH/10) * (i+1) -2
+            line_d.rect.y = 3*(SCREEN_WIDTH/10) + 1
+            
+            all_vertical_list.add(line_c)
+            all_vertical_list.add(line_d)
+            all_horizontal_list.add(line_a)
+            all_horizontal_list.add(line_b)
+            all_blocks_list.add(block)
+        set_first_rows = 0
+# ----x----x----starting blocks-------x------x-----
+
+
+    
+    elif game_screen:
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                  carryOn = False 
+                  
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    print('xx')
+                    main_menu = False
+                    escape_menu = True
+                    
             elif event.type==pygame.KEYDOWN:
                     if event.key==pygame.K_x: 
                          carryOn=False
-    
-            elif event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1 and shoot ==1:
+                         
+                         
+            elif event.type == pygame.MOUSEBUTTONUP :
+                m_x,m_y = event.pos      
+                if m_y > MENU_BAR_HEIGHT and m_x> MENU_BAR_HEIGHT and event.button == 1: 
+                    for ball in all_balls_list:
+                        print('speeding up')
+                        ball.velocity = [ball.velocity[0] *1.5,ball.velocity[1] *1.5]
+                        
+                        
+                elif event.button == 1 and shoot ==1:
                     mouse_x, mouse_y = event.pos 
                     mouse_draging = False
                     y[0] = y[0]
                     y[1] = y[1]
                     draw_line = 0
                     coins = len(all_coins_list)
-                    print(coins)
                     if NUM_of_balls > 0:
                         add_ball([mouse_x, mouse_y],SHOOTING_SPEED,starting_point)
+                        Ball_Launch.play()
                         NUM_of_balls = NUM_of_balls - 1
                     if NUM_of_balls ==0:
                         shoot= 0 
@@ -638,12 +702,11 @@ while carryOn:
                         y[1] = mouse_y
                         draw_line = 1
                     
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
-                m_x,m_y = event.pos      
-                if m_y > MENU_BAR_HEIGHT and m_x> MENU_BAR_HEIGHT: 
-                    for ball in all_balls_list:
-                        ball.velocity = [ball.velocity[0] *1.5,ball.velocity[1] *1.5]
+            
                         
+                        
+
+                
             if shot_already == 1 and not all_balls_list:
                 all_blocks_list.update()
                 all_horizontal_list.update()
@@ -666,7 +729,7 @@ while carryOn:
             all_coins_list.update()
         
     
-        for obj in all_balls_list:
+        for obj in all_balls_list:           
             if obj.rect.x>=SCREEN_WIDTH:
                 obj.velocity[0] = -obj.velocity[0]
             if obj.rect.x<=0:
@@ -678,12 +741,15 @@ while carryOn:
     
         
         for hit in pygame.sprite.groupcollide(all_balls_list,all_blocks_list,0,0):
+            Ball_Hit.play()
             collision_y.add(hit)
             collision_x.add(hit)
             for ball_col in pygame.sprite.groupcollide(collision_y,all_horizontal_list,0,0):
                 hit.velocity[1] = - hit.velocity[1]
+                print('1')
             for ball_col_ in pygame.sprite.groupcollide(collision_x,all_vertical_list,0,0):
                 hit.velocity[0] = - hit.velocity[0]
+                print('1')
             collision_x.empty()
             collision_y.empty()
            
